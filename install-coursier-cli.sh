cf588e85e8cf97ad3816a2e20c40d9d064edfa56
Vladyslav Pekker <agilesteel@gmail.com>
2019-08-08 17:12:41 +0200

    Initial commit

---
+++ install-coursier-cli.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ "$1" == "" ]; then
+    URL="https://git.io/coursier-cli"
+else
+    URL="https://github.com/coursier/coursier/releases/download/v$1/coursier"
+fi
+
+# install coursier
+sudo curl -L $URL -o /usr/local/bin/coursier
+sudo chmod +x /usr/local/bin/coursier
+
+echo
+
+coursier --help
+
+# remove yourself
+rm $0
