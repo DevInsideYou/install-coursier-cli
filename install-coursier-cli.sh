4b0d352e64826e8dfa63f922e8804585bc49cfb9
Vladyslav Pekker <agilesteel@gmail.com>
2020-10-04 16:08:48 -0300

    housekeeping

--- install-coursier-cli.sh
+++ install-coursier-cli.sh
@@ -3,12 +3,6 @@
 # remove yourself
 rm $0
 
-# install curl if necessary
-if [[ -z $(dpkg -l | grep curl) ]] ; then
-  echo 'Installing curl...'
-  sudo apt install -yqqq curl
-fi
-
 # dowbload temporary coursier native-image to bootstrap the installation
 curl -fLo cs https://git.io/coursier-cli-"$(uname | tr LD ld)"
 
